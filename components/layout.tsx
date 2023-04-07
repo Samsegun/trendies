d6318c50b416b8967c12955e6cf2acaa6cb3aabc
Samsegun <oyebadesamsegun@gmail.com>
2023-04-07 22:32:55 +0100

    layout comment

--- components/layout.tsx
+++ components/layout.tsx
@@ -122,7 +122,7 @@ const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
                 signInModal={modal.signIn}
             />
 
-            {/* overlay */}
+            {/* backdrop */}
             {modal.overLay && (
                 <div
                     className='fixed top-0 bottom-0 left-0 right-0 z-30 bg-[rgba(0,0,0,0.4)] '
