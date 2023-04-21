61d11f0f19a0ab6a5d1e776fbf7eda5a465b38f6
Samsegun <oyebadesamsegun@gmail.com>
2023-04-21 22:22:08 +0100

    middleware paused

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,25 @@
+// import { onAuthStateChanged } from "firebase/auth";
 import { NextRequest, NextResponse } from "next/server";
+// import { initialize } from "./firebase";
 
 export default function middleware(req: NextRequest) {
-    let verify = req.cookies.get("accessToken")?.value;
-    let url = req.url;
-
+    // let verify = req.cookies.get("accessToken")?.value;
+    // let url = req.url;
+    // const { auth } = initialize();
+    // onAuthStateChanged(auth, user => {
+    //     if (!user) {
+    //         console.log("he");
+    //         return NextResponse.redirect("https://trendies.vercel.app/login");
+    //     }
+    // });
     // use this if statement when running in dev enviroment
     // if (!verify && url.includes("/checkout")) {
     //     return NextResponse.redirect("http://localhost:3000/login");
     // }
-
     // // use this if statement when running in prod enviroment
-    if (!verify && url.includes("/checkout")) {
-        return NextResponse.redirect("https://trendies.vercel.app/login");
-    }
-
+    // if (!verify && url.includes("/checkout")) {
+    //     return NextResponse.redirect("https://trendies.vercel.app/login");
+    // }
     // if (!verify) {
     //     return NextResponse.redirect("http://localhost:3000/");
     // }
