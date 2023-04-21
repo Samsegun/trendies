3a0c192d68a60199ce6ab2642f9631df6eee2723
Samsegun <oyebadesamsegun@gmail.com>
2023-04-21 20:59:06 +0100

    middleware redirect fixes

--- middleware.ts
+++ middleware.ts
@@ -4,11 +4,19 @@ export default function middleware(req: NextRequest) {
     let verify = req.cookies.get("accessToken")?.value;
     let url = req.url;
 
-    if (!verify && url.includes("/checkout")) {
+    const env = process.env.NODE_ENV;
+
+    // if (env == "development") {
+    //     console.log("dev");
+    // } else if (env == "production") {
+    //     console.log("prod");
+    // }
+
+    if (!verify && url.includes("/checkout") && env == "development") {
         return NextResponse.redirect("http://localhost:3000/login");
     }
 
-    // if (!verify) {
-    //     return NextResponse.redirect("http://localhost:3000/");
-    // }
+    if (!verify && url.includes("/checkout") && env == "production") {
+        return NextResponse.redirect("https://trendies.vercel.app/login");
+    }
 }
