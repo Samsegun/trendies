eb70df92777f33d5bba4d6e74f70b90b63130be3
Samsegun <oyebadesamsegun@gmail.com>
2023-04-22 22:01:47 +0100

    middleware broken

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,8 @@
-// import { onAuthStateChanged } from "firebase/auth";
 import { NextRequest, NextResponse } from "next/server";
-// import { initialize } from "./firebase";
 
 export default function middleware(req: NextRequest) {
     // let verify = req.cookies.get("accessToken")?.value;
     // let url = req.url;
-    // const { auth } = initialize();
-    // onAuthStateChanged(auth, user => {
-    //     if (!user) {
-    //         console.log("he");
-    //         return NextResponse.redirect("https://trendies.vercel.app/login");
-    //     }
-    // });
     // use this if statement when running in dev enviroment
     // if (!verify && url.includes("/checkout")) {
     //     return NextResponse.redirect("http://localhost:3000/login");
